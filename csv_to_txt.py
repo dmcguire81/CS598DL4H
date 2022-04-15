bed84c90e5d4f61239b04d2af1a9a0407ac047bf
Dave McGuire <dmcguire@blackberry.com>
2022-04-15 13:16:06 -0700

    Apply automated formatting

--- csv_to_txt.py
+++ csv_to_txt.py
@@ -1,18 +1,19 @@
-import pandas as pd
 import sys
+
+import pandas as pd
 
 
 def main(caml_mimic_csv_file, hlan_text_file):
     df = pd.read_csv(caml_mimic_csv_file)
     print(df.info())
-    
+
     text_lines = df["TEXT"].tolist()
     label_lines = df["LABELS"].map(lambda line: line.replace(";", " ")).tolist()
-    
+
     with open(hlan_text_file, "w") as fh:
         for text_line, label_line in zip(text_lines, label_lines):
             print(f"{text_line}__label__{label_line}", file=fh)
-        
+
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
--- shielding_icd9.py
+++ shielding_icd9.py
@@ -1,8 +1,11 @@
 import sys
+
 import pandas as pd
+
 
 def insert_period(icd9_code):
     return f"{icd9_code[:3]}.{icd9_code[3:]}" if len(icd9_code) > 3 else icd9_code
+
 
 def main(shielding_csv, mapping_csv, d_icd_diagnoses_csv, diagnosis_icd_csv):
     shielding_df = pd.read_csv(shielding_csv)
@@ -17,19 +20,21 @@ def main(shielding_csv, mapping_csv, d_icd_diagnoses_csv, diagnosis_icd_csv):
     shielding_icd9_df = shielding_icd10_df.join(mapping_df)
     print(shielding_icd9_df.info())
     shielding_icd9_df.to_csv("shielding_icd9.csv", header=True)
-    
+
     unmatched_shiedling_icd9_df = shielding_icd9_df[shielding_icd9_df.ICD9.isnull()]
     print(f"{len(unmatched_shiedling_icd9_df)} unmatched ICD9 codes:")
     print(unmatched_shiedling_icd9_df)
 
     d_icd_diagnoses_df = pd.read_csv(d_icd_diagnoses_csv)
     d_icd_diagnoses_df.set_index("ICD9_CODE", inplace=True)
-    
+
     shielding_icd9_df.set_index("ICD9", inplace=True, drop=False)
     shielding_icd9_df.drop(columns=["CodeType", "TABLETYP"], inplace=True)
     shielding_diagnoses_df = shielding_icd9_df.join(d_icd_diagnoses_df)
     shielding_diagnoses_df.reset_index(drop=True, inplace=True)
-    matched_shielding_diagnoses_df = shielding_diagnoses_df[~shielding_diagnoses_df.ROW_ID.isnull()]
+    matched_shielding_diagnoses_df = shielding_diagnoses_df[
+        ~shielding_diagnoses_df.ROW_ID.isnull()
+    ]
     print()
     print(f"{len(matched_shielding_diagnoses_df)} matched diagnoses.")
     print(matched_shielding_diagnoses_df.info())
@@ -41,15 +46,27 @@ def main(shielding_csv, mapping_csv, d_icd_diagnoses_csv, diagnosis_icd_csv):
     diagnosis_icd_df = pd.read_csv(diagnosis_icd_csv)
     diagnosis_icd_df.drop(columns=["ROW_ID"], inplace=True)
     diagnosis_icd_df.set_index("ICD9_CODE", inplace=True)
-    
-    shielding_diagnoses_prevalence_df = matched_shielding_diagnoses_df[["ICD9"]].join(diagnosis_icd_df[["HADM_ID"]]).groupby(["ICD9"]).nunique()
-    frequent_shielding_diagnoses_df = shielding_diagnoses_prevalence_df[shielding_diagnoses_prevalence_df.HADM_ID > 50]
+
+    shielding_diagnoses_prevalence_df = (
+        matched_shielding_diagnoses_df[["ICD9"]]
+        .join(diagnosis_icd_df[["HADM_ID"]])
+        .groupby(["ICD9"])
+        .nunique()
+    )
+    frequent_shielding_diagnoses_df = shielding_diagnoses_prevalence_df[
+        shielding_diagnoses_prevalence_df.HADM_ID > 50
+    ]
     frequent_shielding_diagnoses_df.reset_index(inplace=True, drop=False)
-    frequent_shielding_diagnoses_df["ICD9"] = frequent_shielding_diagnoses_df["ICD9"].map(insert_period)
+    frequent_shielding_diagnoses_df["ICD9"] = frequent_shielding_diagnoses_df[
+        "ICD9"
+    ].map(insert_period)
     print(frequent_shielding_diagnoses_df[["ICD9"]])
     # Since this is joined with D_ICD_DIAGNOSES.csv and DIAGNOSIS_ICD.csv, results can't get checked in
     # This should go in caml-mimic/mimicdata/mimic3/ along side ALL_CODES.csv and TOP_50_CODES.csv
-    frequent_shielding_diagnoses_df[["ICD9"]].to_csv("SHIELDING_CODES.csv", index=False, header=False)
+    frequent_shielding_diagnoses_df[["ICD9"]].to_csv(
+        "SHIELDING_CODES.csv", index=False, header=False
+    )
+
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
