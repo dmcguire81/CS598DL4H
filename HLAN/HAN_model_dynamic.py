Fix defect in ROC-AUC computation and refactor performance steps

HLAN/HAN_model_dynamic.py
@@ -270,7 +270,9 @@ class HAN:
         with tf.name_scope("dropout"):
             self.h_drop = tf.nn.dropout(document_representation, rate=self.dropout_rate)
 
-        logger.info("5 Logits (linear layer) and Predictions (argmax)")
+        logger.info(
+            "5 Logits (linear layer) and Predictions (sigmoid activation and probability threshold)"
+        )
         with tf.name_scope("output"):
             logits = self.linear_layer()
 

HLAN/prediction.py
@@ -4,10 +4,15 @@ from typing import Any, Callable, Iterable, Mapping
 
 import numpy as np
 import tensorflow as tf
-from sklearn import metrics
 
 from HLAN.HAN_model_dynamic import HAN
-from HLAN.performance import ModelPerformance, RunningModelPerformance
+from HLAN.performance import (
+    ModelOutputs,
+    ModelPerformance,
+    RunningModelPerformance,
+    SummaryPerformance,
+    compute_summary_performance,
+)
 
 
 def predict(
@@ -16,7 +21,7 @@ def predict(
     num_classes: int,
     ckpt_dir: Path,
     feeder: Callable[[], Iterable[Mapping[Any, Any]]],
-):
+) -> ModelOutputs:
     logger = logging.getLogger("prediction")
 
     ckpt_file = ckpt_dir / "checkpoint"
@@ -29,16 +34,26 @@ def predict(
         saver.restore(session, tf.train.latest_checkpoint(ckpt_dir))
 
     running_performance = RunningModelPerformance.empty()
+    all_logits = np.empty((0, num_classes))
     all_predictions = np.empty((0, num_classes))
     all_labels = np.empty((0, num_classes))
 
     for step, feed_dict in enumerate(feeder()):
-        (loss, precision, recall, jaccard_index, predictions, labels,) = session.run(
+        (
+            loss,
+            precision,
+            recall,
+            jaccard_index,
+            logits,
+            predictions,
+            labels,
+        ) = session.run(
             [
                 model.loss,
                 model.precision,
                 model.recall,
                 model.jaccard_index,
+                model.logits,
                 model.predictions,
                 model.input_y,
             ],
@@ -48,33 +63,32 @@ def predict(
         performance = ModelPerformance(
             loss=loss, precision=precision, recall=recall, jaccard_index=jaccard_index
         )
-        logger.debug("Current prediction performance: %s", performance)
+        logger.debug("Current performance: %s", performance)
 
         running_performance = running_performance + performance
         assert running_performance.count == step + 1
 
         if step % 50 == 0:
             logger.info(
-                "Average prediction performance (step %s): %s",
+                "Average performance (step %s): %s",
                 step,
                 running_performance.average(),
             )
 
+        all_logits = np.concatenate((all_logits, logits), axis=0)
         all_predictions = np.concatenate((all_predictions, predictions), axis=0)
         all_labels = np.concatenate((all_labels, labels), axis=0)
 
-    return all_predictions, all_labels
-
-
-def calculate_performance(all_predictions, all_labels):
-    logger = logging.getLogger("calculate_prediction_performance")
-
-    micro_roc_auc_score = metrics.roc_auc_score(
-        all_labels, all_predictions, average="micro"
+    return ModelOutputs(
+        logits=all_logits, predictions=all_predictions, labels=all_labels
     )
-    logger.info("Micro ROC-AUC score is %s", micro_roc_auc_score)
 
-    micro_f1_score = metrics.f1_score(all_labels, all_predictions, average="micro")
-    logger.info("Micro F1 score is %s", micro_f1_score)
 
-    return micro_f1_score, micro_roc_auc_score
+def calculate_performance(model_outputs: ModelOutputs) -> SummaryPerformance:
+    logger = logging.getLogger("calculate_performance")
+    summary_performance = compute_summary_performance(model_outputs)
+
+    logger.info("Micro ROC-AUC score is %s", summary_performance.micro_roc_auc_score)
+    logger.info("Micro F1 score is %s", summary_performance.micro_f1_score)
+
+    return summary_performance

HLAN/validation.py
@@ -1,13 +1,17 @@
 import logging
 from pathlib import Path
-from typing import Any, Callable, Iterable, Mapping, Tuple
+from typing import Any, Callable, Iterable, Mapping
 
 import numpy as np
 import tensorflow as tf
-from sklearn import metrics
 
 from HLAN.HAN_model_dynamic import HAN
-from HLAN.performance import ModelPerformance, RunningModelPerformance
+from HLAN.performance import (
+    ModelOutputs,
+    ModelPerformance,
+    RunningModelPerformance,
+    compute_summary_performance,
+)
 
 
 def validate(
@@ -16,9 +20,10 @@ def validate(
     epoch: int,
     num_classes: int,
     feeder: Callable[[], Iterable[Mapping[Any, Any]]],
-) -> Tuple[np.ndarray, np.ndarray]:
+) -> ModelOutputs:
     logger = logging.getLogger("validation")
     running_performance = RunningModelPerformance.empty()
+    all_logits = np.empty((0, num_classes))
     all_predictions = np.empty((0, num_classes))
     all_labels = np.empty((0, num_classes))
 
@@ -30,6 +35,7 @@ def validate(
             precision,
             recall,
             jaccard_index,
+            logits,
             predictions,
             labels,
         ) = session.run(
@@ -40,6 +46,7 @@ def validate(
                 model.precision,
                 model.recall,
                 model.jaccard_index,
+                model.logits,
                 model.predictions,
                 model.input_y,
             ],
@@ -67,6 +74,7 @@ def validate(
         if step == 0:  # epoch rolled over
             model.writer.add_summary(validation_loss_per_epoch, epoch)
 
+        all_logits = np.concatenate((all_logits, logits), axis=0)
         all_predictions = np.concatenate((all_predictions, predictions), axis=0)
         all_labels = np.concatenate((all_labels, labels), axis=0)
 
@@ -77,7 +85,9 @@ def validate(
         running_performance.average(),
     )
 
-    return all_predictions, all_labels
+    return ModelOutputs(
+        logits=all_logits, predictions=all_predictions, labels=all_labels
+    )
 
 
 def update_performance(
@@ -86,31 +96,31 @@ def update_performance(
     session: tf.compat.v1.Session,
     best_micro_f1_score: float,
     epoch: int,
-    all_predictions: np.ndarray,
-    all_labels: np.ndarray,
+    model_outputs: ModelOutputs,
 ):
     logger = logging.getLogger("update_performance")
 
-    micro_roc_auc_score = metrics.roc_auc_score(
-        all_labels, all_predictions, average="micro"
-    )
-    logger.info("Micro ROC-AUC score is %s", micro_roc_auc_score)
+    summary_performance = compute_summary_performance(model_outputs)
+    logger.info("Micro ROC-AUC score is %s", summary_performance.micro_roc_auc_score)
 
-    micro_f1_score = metrics.f1_score(all_labels, all_predictions, average="micro")
     current_learning_rate = session.run(model.learning_rate)
 
-    if micro_f1_score >= best_micro_f1_score:
+    if summary_performance.micro_f1_score >= best_micro_f1_score:
         logger.info(
-            "Micro F1 score improved from %s to %s", best_micro_f1_score, micro_f1_score
+            "Micro F1 score improved from %s to %s",
+            best_micro_f1_score,
+            summary_performance.micro_f1_score,
         )
         saver = tf.compat.v1.train.Saver(max_to_keep=1)
         save_path = ckpt_dir / "model.ckpt"
         logger.info("Saving model checkpoint to %s", save_path)
         saver.save(session, str(save_path), global_step=epoch)
-        best_micro_f1_score = micro_f1_score
+        best_micro_f1_score = summary_performance.micro_f1_score
     else:
         logger.info(
-            "Micro F1 score degraded from %s to %s", best_micro_f1_score, micro_f1_score
+            "Micro F1 score degraded from %s to %s",
+            best_micro_f1_score,
+            summary_performance.micro_f1_score,
         )
         _ = session.run([model.learning_rate_decay_half_op])
         new_learning_rate = session.run(model.learning_rate)

tests/test_validation.py
@@ -1,174 +1,11 @@
 from pathlib import Path
-from typing import List, Type
 
 import numpy as np
 import pytest
-import tensorflow as tf
-from gensim.models import Word2Vec
 from pytest_mock import MockerFixture
-from sklearn import metrics
 
-from HLAN import data_feeding, data_loading, session_creation, validation
-from HLAN.HAN_model_dynamic import HA_GRU, HAN, HLAN
-
-
-def intersect_size(yhat, y, axis):
-    return np.logical_and(yhat, y).sum(axis=axis).astype(float)
-
-
-def micro_precision(yhatmic, ymic):
-    return intersect_size(yhatmic, ymic, 0) / yhatmic.sum(axis=0)
-
-
-def micro_recall(yhatmic, ymic):
-    return intersect_size(yhatmic, ymic, 0) / ymic.sum(axis=0)
-
-
-def micro_f1(yhatmic, ymic):
-    prec = micro_precision(yhatmic, ymic)
-    rec = micro_recall(yhatmic, ymic)
-    if prec + rec == 0:
-        f1 = 0.0
-    else:
-        f1 = 2 * (prec * rec) / (prec + rec)
-    return f1
-
-
-def auc_metrics(yhat_raw, y, ymic):
-    if yhat_raw.shape[0] <= 1:
-        return
-    fpr = {}
-    tpr = {}
-    roc_auc = {}
-    # get AUC for each label individually
-    relevant_labels = []
-    auc_labels = {}
-    for i in range(y.shape[1]):
-        # only if there are true positives for this label
-        if y[:, i].sum() > 0:  # if the label has a true instance in the data
-            fpr[i], tpr[i], _ = metrics.roc_curve(y[:, i], yhat_raw[:, i])
-            if len(fpr[i]) > 1 and len(tpr[i]) > 1:
-                auc_score = metrics.auc(fpr[i], tpr[i])
-                if not np.isnan(auc_score):
-                    auc_labels["auc_%d" % i] = auc_score
-                    relevant_labels.append(i)
-
-    # macro-AUC: just average the auc scores
-    aucs = []
-    for i in relevant_labels:
-        aucs.append(auc_labels["auc_%d" % i])
-    roc_auc["auc_macro"] = np.mean(aucs)
-
-    # micro-AUC: just look at each individual prediction
-    yhatmic = yhat_raw.ravel()
-    fpr["micro"], tpr["micro"], _ = metrics.roc_curve(ymic, yhatmic)
-    roc_auc["auc_micro"] = metrics.auc(fpr["micro"], tpr["micro"])
-
-    return roc_auc
-
-
-@pytest.mark.slow()
-@pytest.mark.parametrize(
-    ("per_label_attention", "per_label_sent_only", "model_class"),
-    [
-        (False, False, HAN),
-        (True, True, HA_GRU),
-        (True, False, HLAN),
-    ],
-)
-def test_validation_performance(
-    monkeypatch: pytest.MonkeyPatch,
-    mocker: MockerFixture,
-    caml_dataset_paths: List[Path],
-    word2vec_model_path: Path,
-    word2vec_model: Word2Vec,
-    batch_size: int,
-    sequence_length: int,
-    num_sentences: int,
-    empty_ckpt_dir: Path,
-    remove_ckpts_before_train: bool,
-    label_embedding_model_path: Path,
-    label_embedding_model_path_per_label: Path,
-    tmp_path: Path,
-    per_label_attention: bool,
-    per_label_sent_only: bool,
-    model_class: Type,
-):
-    validation_data_path, testing_data_path, training_data_path = sorted(
-        caml_dataset_paths, key=str
-    )
-
-    monkeypatch.setattr(
-        data_loading.Word2Vec, "load", mocker.MagicMock(return_value=word2vec_model)
-    )
-    onehot_encoding = data_loading.load_encoding_data(
-        word2vec_model_path, validation_data_path, training_data_path, testing_data_path
-    )
-    monkeypatch.undo()
-    (onehot_encoding, training_data_split,) = data_loading.load_data(
-        word2vec_model_path,
-        validation_data_path,
-        training_data_path,
-        testing_data_path,
-        sequence_length=sequence_length,
-    )
-
-    with tf.compat.v1.variable_scope(
-        f"test_validation_performance-{per_label_attention}-{per_label_sent_only}"
-    ):
-        model = model_class(
-            num_classes=onehot_encoding.num_classes,
-            learning_rate=0.01,
-            batch_size=batch_size,
-            decay_steps=6000,
-            decay_rate=1.0,
-            sequence_length=sequence_length,
-            num_sentences=num_sentences,
-            vocab_size=onehot_encoding.vocab_size,
-            embed_size=100,
-            hidden_size=100,
-            log_dir=tmp_path,
-        )
-
-        with session_creation.create_session(
-            model=model,
-            ckpt_dir=empty_ckpt_dir,
-            remove_ckpts_before_train=remove_ckpts_before_train,
-            per_label_attention=False,
-            per_label_sent_only=False,
-            reverse_embedding=onehot_encoding.reverse,
-            word2vec_model_path=word2vec_model_path,
-            label_embedding_model_path=label_embedding_model_path,
-            label_embedding_model_path_per_label=label_embedding_model_path_per_label,
-            use_label_embedding=True,
-        ) as session:
-            epoch = 0
-
-            all_predictions, all_labels = validation.validate(
-                session,
-                model,
-                epoch,
-                onehot_encoding.num_classes,
-                lambda: data_feeding.feed_data(
-                    model, *training_data_split.validation, batch_size=batch_size
-                ),
-            )
-
-        sklearn_micro_f1_score = metrics.f1_score(
-            all_labels, all_predictions, average="micro"
-        )
-        original_micro_f1_score = micro_f1(all_predictions.ravel(), all_labels.ravel())
-
-        assert sklearn_micro_f1_score == original_micro_f1_score
-
-        sklearn_micro_roc_auc_score = metrics.roc_auc_score(
-            all_labels, all_predictions, average="micro"
-        )
-        original_micro_roc_auc_score = auc_metrics(
-            all_predictions, all_labels, all_labels.ravel()
-        )["auc_micro"]
-
-        assert sklearn_micro_roc_auc_score == original_micro_roc_auc_score
+from HLAN import validation
+from HLAN.performance import ModelOutputs, SummaryPerformance
 
 
 def test_update_validation_performance_saves_checkpoint_on_improvement(
@@ -179,21 +16,28 @@ def test_update_validation_performance_saves_checkpoint_on_improvement(
     mock_model = mocker.MagicMock()
     mock_session = mocker.MagicMock()
 
-    mock_roc_auc_score = mocker.MagicMock()
-    monkeypatch.setattr(validation.metrics, "roc_auc_score", mock_roc_auc_score)
-
     # performance has improved
     best_micro_f1_score = 0
-    mock_f1_score = mocker.MagicMock(return_value=1)
-    monkeypatch.setattr(validation.metrics, "f1_score", mock_f1_score)
+    mock_summary_performance = SummaryPerformance(
+        micro_f1_score=1, micro_roc_auc_score=1
+    )
+    mock_compute_summary_performance = mocker.MagicMock(
+        return_value=mock_summary_performance
+    )
+    monkeypatch.setattr(
+        validation, "compute_summary_performance", mock_compute_summary_performance
+    )
 
     mock_saver = mocker.MagicMock()
     mock_saver_constructor = mocker.MagicMock(return_value=mock_saver)
     monkeypatch.setattr(validation.tf.compat.v1.train, "Saver", mock_saver_constructor)
 
     epoch = 0
-    all_predictions = np.empty((0, 50))
-    all_labels = np.empty((0, 50))
+    model_outputs = ModelOutputs(
+        logits=np.empty((0, 50)),
+        predictions=np.empty((0, 50)),
+        labels=np.empty((0, 50)),
+    )
 
     best_micro_f1_score, _ = validation.update_performance(
         empty_ckpt_dir,
@@ -201,24 +45,13 @@ def test_update_validation_performance_saves_checkpoint_on_improvement(
         mock_session,
         best_micro_f1_score,
         epoch,
-        all_predictions,
-        all_labels,
+        model_outputs,
     )
 
     # updated
     assert best_micro_f1_score == 1
 
-    mock_roc_auc_score.assert_called_once_with(
-        all_labels,
-        all_predictions,
-        average="micro",
-    )
-
-    mock_f1_score.assert_called_once_with(
-        all_labels,
-        all_predictions,
-        average="micro",
-    )
+    mock_compute_summary_performance.assert_called_once_with(model_outputs)
 
     mock_saver.save.assert_called_once_with(
         mock_session,
@@ -235,17 +68,24 @@ def test_update_validation_performance_halves_learning_rate_on_degredation(
     mock_model = mocker.MagicMock()
     mock_session = mocker.MagicMock()
 
-    mock_roc_auc_score = mocker.MagicMock()
-    monkeypatch.setattr(validation.metrics, "roc_auc_score", mock_roc_auc_score)
-
     # performance has degraded
     best_micro_f1_score = 1
-    mock_f1_score = mocker.MagicMock(return_value=0)
-    monkeypatch.setattr(validation.metrics, "f1_score", mock_f1_score)
+    mock_summary_performance = SummaryPerformance(
+        micro_f1_score=0, micro_roc_auc_score=0
+    )
+    mock_compute_summary_performance = mocker.MagicMock(
+        return_value=mock_summary_performance
+    )
+    monkeypatch.setattr(
+        validation, "compute_summary_performance", mock_compute_summary_performance
+    )
 
     epoch = 0
-    all_predictions = np.empty((0, 50))
-    all_labels = np.empty((0, 50))
+    model_outputs = ModelOutputs(
+        logits=np.empty((0, 50)),
+        predictions=np.empty((0, 50)),
+        labels=np.empty((0, 50)),
+    )
 
     best_micro_f1_score, _ = validation.update_performance(
         empty_ckpt_dir,
@@ -253,24 +93,13 @@ def test_update_validation_performance_halves_learning_rate_on_degredation(
         mock_session,
         best_micro_f1_score,
         epoch,
-        all_predictions,
-        all_labels,
+        model_outputs,
     )
 
     # not updated
     assert best_micro_f1_score == 1
 
-    mock_roc_auc_score.assert_called_once_with(
-        all_labels,
-        all_predictions,
-        average="micro",
-    )
-
-    mock_f1_score.assert_called_once_with(
-        all_labels,
-        all_predictions,
-        average="micro",
-    )
+    mock_compute_summary_performance.assert_called_once_with(model_outputs)
 
     mock_session.run.assert_has_calls(
         [